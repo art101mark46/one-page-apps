e9e32921e025a0e624cca86442fcf2e4008fb9f9
art101mark46 <83919187+art101mark46@users.noreply.github.com>
2026-06-21 17:08:33 +0100

    Update dictionary.js
    Some new words to pick up from videos

--- bsl-trainer/dictionary.js
+++ bsl-trainer/dictionary.js
@@ -1,8 +1,8 @@
 // dictionary.js
 export const dictionary = {
-    easy: ["CAT", "DOG", "TEA", "SAM", "BEN", "MAM", "DAD", "TAP", "HEN", "TEN", "TIN", "TIP", "PIT", "PAT", "PET"], // 'Sam' and 'Ben' will be fingerspelt automatically
-    medium: ["COFFEE", "WATER", "LONDON", "ALEX", "DAVID", "DANNY", "KATE", "BATH", "TYNE", "TEES", "WEAR"], 
-    hard: ["BICYCLE", "COMPUTER", "SCOTLAND", "CHLOE", "SARAH", "MATTHEW", "ANDREW", "TAKEAWAY", "CHICKEN", "POTATO", "TOMATO", "LASAGNE", "CONTAINER", "NEWCASTLE", "SUNDERLAND"]
+    easy: ["ORANGE", "BLUE", "CAT", "DOG", "TEA", "SAM", "BEN", "MAM", "DAD", "TAP", "HEN", "TEN", "TIN", "TIP", "PIT", "PAT", "PET"], // 'Sam' and 'Ben' will be fingerspelt automatically
+    medium: ["HELP YOU", "CHARITY", "HELP ME", "COFFEE", "WATER", "LONDON", "INTERPRETER", "ALEX", "DAVID", "DANNY", "KATE", "BATH", "TYNE", "TEES", "WEAR"], 
+    hard: ["BICYCLE", "COMPUTER", "ORGANISATION", "VOLUNTEER", "SCOTLAND", "CHLOE", "SARAH", "MATTHEW", "ANDREW", "TAKEAWAY", "CHICKEN", "POTATO", "TOMATO", "LASAGNE", "CONTAINER", "NEWCASTLE", "SUNDERLAND"]
 };
 
 /**
