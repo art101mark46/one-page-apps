55b9a7f3696802633a547c6fe2fecf72cef48dcb
art101mark46 <83919187+art101mark46@users.noreply.github.com>
2026-06-21 17:16:09 +0100

    Update dictionary.js
    fixed help you and help me entries

--- bsl-trainer/dictionary.js
+++ bsl-trainer/dictionary.js
@@ -1,7 +1,7 @@
 // dictionary.js
 export const dictionary = {
     easy: ["ORANGE", "BLUE", "CAT", "DOG", "TEA", "SAM", "BEN", "MAM", "DAD", "TAP", "HEN", "TEN", "TIN", "TIP", "PIT", "PAT", "PET"], // 'Sam' and 'Ben' will be fingerspelt automatically
-    medium: ["HELP YOU", "CHARITY", "HELP ME", "COFFEE", "WATER", "LONDON", "INTERPRETER", "ALEX", "DAVID", "DANNY", "KATE", "BATH", "TYNE", "TEES", "WEAR"], 
+    medium: ["HELP-YOU", "CHARITY", "HELP-ME", "COFFEE", "WATER", "LONDON", "INTERPRETER", "ALEX", "DAVID", "DANNY", "KATE", "BATH", "TYNE", "TEES", "WEAR"], 
     hard: ["BICYCLE", "COMPUTER", "ORGANISATION", "VOLUNTEER", "SCOTLAND", "CHLOE", "SARAH", "MATTHEW", "ANDREW", "TAKEAWAY", "CHICKEN", "POTATO", "TOMATO", "LASAGNE", "CONTAINER", "NEWCASTLE", "SUNDERLAND"]
 };
 
